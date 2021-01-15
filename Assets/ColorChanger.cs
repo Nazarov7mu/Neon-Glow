8d3fef757ff6ed9251c51ef4750d75cfb4be6f72
Javokhir Nazarov <nazarov7mu@gmail.com>
2021-01-15 18:13:09 +0900

    Comments and Minor Changes

--- Assets/ColorChanger.cs
+++ Assets/ColorChanger.cs
@@ -3,21 +3,18 @@
 public class ColorChanger : MonoBehaviour
 {
     private Material _material;
-
-    private readonly string _colorName = "_GlowColor";
+    private readonly string _colorName = "_GlowColor"; //Should be exactly the same as in Shader Graph
     private Color32 _defaultColor;
 
 
     private void Start()
     {
-        _material = GetComponent<SpriteRenderer>().material;
+        _material = GetComponent<SpriteRenderer>().material; //get the material attached to our Sprite
 
         Color32 color = new Color32(255, 255, 0, 0);
 
         _material.SetColor(_colorName, color);
         _defaultColor = color;
-
-        
     }
 
     private void Update()
@@ -25,11 +22,9 @@ public class ColorChanger : MonoBehaviour
         Color32 newColor = new Color32(0, 255, 255, 0);
         Color32 lerpedColor;
 
+        //Script Reference: https://docs.unity3d.com/ScriptReference/Color.Lerp.html
         lerpedColor = Color32.Lerp(_defaultColor, newColor, Mathf.PingPong(Time.time, 1));
 
         _material.SetColor(_colorName, lerpedColor);
-       
     }
-    
-
 }
