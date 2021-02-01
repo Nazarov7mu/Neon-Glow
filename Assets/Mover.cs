f8049d9c075ded6f7c02a8c32d11b8fafbbe2653
Javokhir Nazarov <nazarov7mu@gmail.com>
2021-02-01 13:30:11 +0900

    Experimenting with different assets and glow

---
+++ Assets/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private float speed = 0.5f;
+    Vector3 pointA;
+    Vector3 pointB;
+
+    void Start()
+    {
+        pointA = transform.position;
+        pointB = new Vector3(transform.position.x, transform.position.y+4, 0);
+    }
+
+    void Update()
+    {
+        //PingPong between 0 and 1
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+    }
+}
